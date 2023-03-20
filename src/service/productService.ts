68f20037670194ca321140a291acc1d5481cb6c4
Marcio Junior <marcio.jr1408@gmail.com>
2023-03-20 00:28:26 -0300

    AEP

---
+++ src/service/productService.ts
@@ -0,0 +1,51 @@
+import { writeFile, readFile } from 'fs/promises'
+ 
+
+
+class productService{
+
+
+    async createProductList(data){
+        try{
+        await writeFile('products.json', JSON.stringify(data, null, 2))
+
+        } catch(error){
+            throw new Error(error)
+        }
+
+
+
+    }
+    async listProducts(){
+        try {
+            const productList = await readFile('products.json', "utf-8")
+
+
+            return JSON.parse(productList)  
+        }
+        catch(error){
+            console.error(error)
+        }
+    }
+
+    async getStock(){
+        const productList =  await this.listProducts()
+
+        const stockProducts = productList.map(item => {
+            let stock = {
+                nome: item.nome,
+                qtde: item.qtde,
+                preco: item.preco,
+                valor_stock: item.qtde * item.preco
+            }
+            return stock
+        })
+        return stockProducts
+    }
+
+
+
+}
+
+
+export default new productService
